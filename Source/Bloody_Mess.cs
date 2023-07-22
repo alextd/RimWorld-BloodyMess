85dc973db87ba31550b936fe05163693e71f948f
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2023-07-22 00:32:14 -0700

    Initial Commit

---
+++ Source/Bloody_Mess.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Bloody_Mess
+{
+	public class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+#if DEBUG
+			Harmony.DEBUG = true;
+#endif
+
+			Harmony harmony = new("Uuugggg.rimworld.Bloody_Mess.main");	
+			harmony.PatchAll();
+		}
+	}
+}
